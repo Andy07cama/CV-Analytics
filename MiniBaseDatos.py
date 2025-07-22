fc0f74a5723294ce948680cf68812be8b4c6f97e
Andy07cama <47961283@est.ort.edu.ar>
2025-07-22 17:23:18 -0300

    borre api gemini para q no se vea

--- MiniBaseDatos.py
+++ MiniBaseDatos.py
@@ -12,7 +12,7 @@ os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
 try:
     
-    API_KEY = "AIzaSyDda1OWyAddH2h5p3Aolgxek7ISQtpXeIw" 
+    API_KEY = "" 
     
     if not API_KEY or API_KEY == "TU_NUEVA_API_KEY":
         print("ADVERTENCIA: No se ha configurado una clave de API de Gemini en MiniBaseDatos.py.")
